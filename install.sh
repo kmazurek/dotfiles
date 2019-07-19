dbf2a8793e40203518f6aee2e0f26e330225ce19
Kuba Mazurek <jakub.mazurek@golem.network>
2019-07-19 14:04:50 +0200

    Install script: downloading stowsh to temp dir

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+WORKING_DIR=$(mktemp -d)
+STOWSH_REPO="Https://raw.githubusercontent.com/williamsmj/stowsh/master/stowsh"
+
 # Get stowsh, add it to path
-wget -P ~/.local/bin https://raw.githubusercontent.com/williamsmj/stowsh/master/stowsh
-chmod +x ~/.local/bin/stowsh
+wget -P "$WORKING_DIR" "$STOWSH_REPO"
+stowsh_path="$WORKING_DIR/stowsh"
+chmod +x "$stowsh_path"
 
 # Get root level dirs that don't start with a dot
 pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
@@ -15,6 +19,8 @@ do
 
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        stowsh -v -s "$pkg" -t "$HOME"
+        "$stowsh_path" -v -s "$pkg" -t "$HOME"
     fi
 done
+
+rm -r "$WORKING_DIR"
