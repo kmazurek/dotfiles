22b78a2840a53a0c02253d6e04a5517c56f70065
Kuba Mazurek <zakaprov@gmail.com>
2019-10-23 16:24:08 +0200

    ranger: created zsh submodule

---
+++ ranger/.zsh/ranger.sh
@@ -0,0 +1,9 @@
+function ranger-cd {
+    tempfile="$(mktemp -t tmp.XXXXXX)"
+    /usr/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    test -f "$tempfile" &&
+    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+        cd -- "$(cat "$tempfile")"
+    fi
+    rm -f -- "$tempfile"
+}
