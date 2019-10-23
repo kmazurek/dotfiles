fae2ba7f927f56c2a706bd296d5e0c7b21a2550a
Kuba Mazurek <zakaprov@gmail.com>
2019-10-23 14:10:31 +0200

    Reorganised install script

--- install.sh
+++ install.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
 WORKING_DIR=$(mktemp -d)
+STOWSH_PATH="$WORKING_DIR/stowsh"
 STOWSH_REPO="Https://raw.githubusercontent.com/williamsmj/stowsh/master/stowsh"
 
-# Get stowsh, add it to path
-wget -P "$WORKING_DIR" "$STOWSH_REPO"
-stowsh_path="$WORKING_DIR/stowsh"
-chmod +x "$stowsh_path"
+get_stowsh() {
+    wget -P "$WORKING_DIR" "$STOWSH_REPO"
+    chmod +x "$STOWSH_PATH"
+}
 
-# Get root level dirs that don't start with a dot
-pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
-
-# Install packages
-for pkg in $pkgs
-do
-    read -p "Link $pkg? (y/n) " -n 1 -r
+link_package() {
+    read -p "Link $1? (y/n) " -n 1 -r
     echo
 
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        "$stowsh_path" -v -s "$pkg" -t "$HOME"
+        "$STOWSH_PATH" -v -s "$1" -t "$HOME"
     fi
-done
+}
 
-rm -r "$WORKING_DIR"
+main() {
+    # Download stowsh to a temporary directory
+    get_stowsh
+
+    # Get root level dirs that don't start with a dot
+    pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
+
+    # Install packages
+    for pkg in $pkgs
+    do
+        link_package $pkg
+    done
+
+    rm -r "$WORKING_DIR"
+}
+
+main
