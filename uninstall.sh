f5481dbbf224ebdc1018a6a5cb6f86e4ad130597
Kuba Mazurek <zakaprov@gmail.com>
2019-11-27 18:19:27 +0100

    Passing in package names to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Get root level dirs that don't start with a dot
-pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
+# If any arguments were passed in, use them as the list of packages to install
+if [ $# -eq 0 ]; then
+    # Get root level dirs that don't start with a dot
+    pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
+else
+    pkgs="$@"
+fi
 
-# Uninstall packages
 for pkg in $pkgs
 do
     read -p "Unlink $pkg? (y/n) " -n 1 -r
