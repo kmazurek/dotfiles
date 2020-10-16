13b97bf04fc9b1c2177af1e6531dd86d3c0d9458
Kuba Mazurek <zakaprov@gmail.com>
2020-10-16 18:43:29 +0200

    Add dotnet telemetry optout env variable

--- zsh/.zsh/config.sh
+++ zsh/.zsh/config.sh
@@ -2,9 +2,10 @@ export PASSWORD_STORE_GPG_OPTS='--no-throw-keyids'
 
 [ -f ~/.config/up/up.sh ] && source ~/.config/up/up.sh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 export RIPGREP_CONFIG_PATH=~/.rgrc
 
 # Disables XON/XOFF flow control (C-s hanging terminal)
 stty -ixon
 
-
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
