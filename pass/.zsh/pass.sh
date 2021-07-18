e915fc77ace7e9a639efce449a6bd1457e839c76
Kuba Mazurek <zakaprov@gmail.com>
2021-07-18 15:36:44 +0200

    Simplify misc zsh configs

--- pass/.zsh/pass.sh
+++ pass/.zsh/pass.sh
@@ -1,4 +1,5 @@
 export PASSWORD_STORE_ENABLE_EXTENSIONS=true
+export PASSWORD_STORE_GPG_OPTS='--no-throw-keyids'
 
 function pass_wrapper() {
     if [ $# -eq 0 ]; then
--- zsh/.zsh/config.sh
+++ zsh/.zsh/config.sh
@@ -1,8 +1,3 @@
-export PASSWORD_STORE_GPG_OPTS='--no-throw-keyids'
-
-[ -f ~/.config/up/up.sh ] && source ~/.config/up/up.sh
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 export RIPGREP_CONFIG_PATH=~/.rgrc
 
 # Disables XON/XOFF flow control (C-s hanging terminal)
