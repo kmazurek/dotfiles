72a4a64064ce91366496af031cce8fab6b8b74c5
Kuba Mazurek <zakaprov@gmail.com>
2021-07-18 15:35:50 +0200

    Only install shell in zsh installer

--- .installers/zsh/install.sh
+++ .installers/zsh/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-sudo apt install zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
+$INSTALL_CMD zsh
+chsh -s $(which zsh)
