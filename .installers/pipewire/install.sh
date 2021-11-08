c9577752bdf0728bf583646847304c9227ee8579
Kuba Mazurek <zakaprov@gmail.com>
2021-11-08 23:58:06 +0100

    Update systemctl call in pipewire installer

--- .installers/pipewire/install.sh
+++ .installers/pipewire/install.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 $INSTALL_CMD pipewire pipewire-alsa pipewire-pulseaudio pipewire-libjack-0_3
-systemctl --user enable --now pipewire.{service,socket}
+systemctl --user enable --now pipewire-media-session.service
