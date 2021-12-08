bf9c8ce47caef67178ea854c3535562913f56630
Kuba Mazurek <zakaprov@gmail.com>
2021-12-08 09:57:01 +0100

    Create mock pulseaudio binary when installing pipewire

--- .installers/pipewire/install.sh
+++ .installers/pipewire/install.sh
@@ -3,3 +3,7 @@ set -euo pipefail
 
 $INSTALL_CMD pipewire pipewire-alsa pipewire-pulseaudio pipewire-libjack-0_3
 systemctl --user enable --now pipewire-media-session.service
+
+# Provide mock pulseaudio binary for programs which rely on checking it (e.g. FMOD Studio)
+mkdir -p ~/.local/bin
+ln -s /bin/true ~/.local/bin/pulseaudio
