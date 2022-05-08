a7f609b89c80c86ed210cbbe11329a0ecef66c5b
Kuba Mazurek <zakaprov@gmail.com>
2022-05-08 15:54:35 +0200

    Update Unity installer

--- .installers/unity/install.sh
+++ .installers/unity/install.sh
@@ -6,3 +6,6 @@ opi mono-complete
 opi dotnet-sdk
 
 # After installing a code editor, select it in Unity's Edit->Preferences->External Tools and regenerate project files
+
+# As of Unity 2020.3.x, libicu version below 7 is required, go for libicu69 for example
+opi libicu
