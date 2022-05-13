30dc25aa55c3c7dea931a8ee5dd4ab8727884112
Kuba Mazurek <zakaprov@gmail.com>
2022-05-13 18:47:27 +0200

    Change git pager to delta

--- .installers/git/install.sh
+++ .installers/git/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-curl --create-dirs -o ~/.local/bin/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
-chmod +x ~/.local/bin/diff-so-fancy
+$INSTALL_CMD git-delta
