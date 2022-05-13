373d9b29908b458851fcb505f9d126d133981f8c
Kuba Mazurek <zakaprov@gmail.com>
2022-05-13 18:36:44 +0200

    Add zsh long history config

--- zsh/.zsh/config.sh
+++ zsh/.zsh/config.sh
@@ -4,3 +4,16 @@ export RIPGREP_CONFIG_PATH=~/.rgrc
 stty -ixon
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+# Set max history size
+export HISTSIZE=10000
+# Save history after logout
+export SAVEHIST=10000
+# Set history file explicitly
+export HISTFILE=~/.zsh_history
+# Append to history file
+setopt INC_APPEND_HISTORY
+# Do not save a command when there's a duplicate in history
+setopt HIST_IGNORE_DUPS
+# Add timestamp for each history entry
+setopt EXTENDED_HISTORY   
