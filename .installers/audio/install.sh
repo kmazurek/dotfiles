7fc3dd0bf6186634716af8836a37b00a63c6a8ff
Kuba Mazurek <zakaprov@gmail.com>
2022-06-08 17:18:36 +0200

    Add yabridge to audio installer

--- .installers/audio/install.sh
+++ .installers/audio/install.sh
@@ -7,3 +7,5 @@ sudo cp audio.conf /etc/security/limits.d
 echo "fs.inotify.max_user_watches = 524288" | sudo tee /etc/sysctl.conf
 
 $INSTALL_CMD qjackctl
+# See: https://geekosdaw.tuxfamily.org/en/ in case yabridge is not found
+$INSTALL_CMD yabridge yabridgectl wine
