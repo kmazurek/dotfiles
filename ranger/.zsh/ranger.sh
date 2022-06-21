4660be06e4c2412cb7ff375d9e8c2364c5ef311c
Kuba Mazurek <zakaprov@gmail.com>
2022-06-21 14:48:17 +0200

    Replace hardcoded path in ranger-cd

--- ranger/.zsh/ranger.sh
+++ ranger/.zsh/ranger.sh
@@ -1,6 +1,6 @@
 function ranger-cd {
     tempfile="$(mktemp -t tmp.XXXXXX)"
-    /usr/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    $(which -a ranger | tail -n 1) --choosedir="$tempfile" "${@:-$(pwd)}"
     test -f "$tempfile" &&
     if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
         cd -- "$(cat "$tempfile")"
