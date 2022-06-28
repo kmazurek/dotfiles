1a71dfa48f11370358af397c2e1b71676bc6be9d
Kuba Mazurek <zakaprov@gmail.com>
2022-06-28 09:59:32 +0200

    Update pass installer

--- .installers/pass/install.sh
+++ .installers/pass/install.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 
 case "$DISTRO" in
     *openSUSE*)
-        $INSTALL_CMD password-store
-        $INSTALL_CMD pinentry-gtk2
+        $INSTALL_CMD password-store pinentry-gtk2
         ;;
     *)
         $INSTALL_CMD pass
@@ -14,6 +13,6 @@ esac
 $INSTALL_CMD fzf
 
 key="0x53507E32EE45F70D"
-gpg --keyserver pgp.mit.edu --recv-key $key
+gpg --recv-key $key
 # used to set ultimate trust for the imported key
 gpg --edit-key $key
