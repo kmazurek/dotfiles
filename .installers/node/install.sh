cb50af44fa87e28f0995592cea44109adbe941e5
Kuba Mazurek <zakaprov@gmail.com>
2022-07-08 10:40:22 +0200

    Fix Node installer script

--- .installers/node/install.sh
+++ .installers/node/install.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # Install node and npm for the current user using the n version manager
+mkdir -p "~/.local/bin"
 n_destination=$(realpath ~/.local/bin/n)
 mkdir -p $n_destination
 curl -L https://git.io/n-install | N_PREFIX=$n_destination bash -s -- -n
