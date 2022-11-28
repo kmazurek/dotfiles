f03a829649eb06b8fde99b39b24446ef9d9aba46
Kuba Mazurek <zakaprov@gmail.com>
2022-11-28 09:43:49 +0100

    macos: add defaults calls to install script

--- .installers/macos/install.sh
+++ .installers/macos/install.sh
@@ -14,3 +14,12 @@ brew install findutils
 PATH=$(brew --prefix)/opt/findutils/libexec/gnubin:$PATH
 
 brew install openssl gpg-suite pinentry-mac
+
+# Disable special characters pop-up when holding down a key
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Show full path in the Finder header
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+
+# Sort directories first in Finder
+defaults write com.apple.finder _FXSortFoldersFirst -bool true; killall Finder
