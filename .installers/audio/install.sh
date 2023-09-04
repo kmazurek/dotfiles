85a4e49c8e222120cb8a84a0b48aa559fe568061
Kuba Mazurek <zakaprov@gmail.com>
2023-09-04 20:50:09 +0200

    Update RT audio config

--- .installers/audio/install.sh
+++ .installers/audio/install.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 sudo usermod -aG audio "$USER"
 sudo cp audio.conf /etc/security/limits.d
 
-echo "fs.inotify.max_user_watches = 524288" | sudo tee /etc/sysctl.conf
+# https://wiki.linuxaudio.org/wiki/system_configuration#quality_of_service_interface
+sudo cp 99-cpu-dma-latency.rules /etc/udev/rules.d
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+sudo cp sysctl.conf /etc/sysctl.conf
 
 $INSTALL_CMD qjackctl
 # See: https://geekosdaw.tuxfamily.org/en/ in case yabridge is not found
