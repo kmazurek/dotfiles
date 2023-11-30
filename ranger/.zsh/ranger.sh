ed79d3f830677123d36152fb6f3c0f03c4900f06
Kuba Mazurek <zakaprov@gmail.com>
2023-11-30 21:07:04 +0100

    Update ranger config and keymap

--- ranger/.zsh/ranger.sh
+++ ranger/.zsh/ranger.sh
@@ -1,11 +1,26 @@
 function ranger-cd {
-    tempfile="$(mktemp -t tmp.XXXXXX)"
-    $(which -a ranger | tail -n 1) --choosedir="$tempfile" "${@:-$(pwd)}"
-    test -f "$tempfile" &&
-    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
-        cd -- "$(cat "$tempfile")"
-    fi
-    rm -f -- "$tempfile"
+	tempfile="$(mktemp -t tmp.XXXXXX)"
+	$(which -a ranger | tail -n 1) --choosedir="$tempfile" "${@:-$(pwd)}"
+	test -f "$tempfile" &&
+		if [ "$(cat -- "$tempfile")" != "$(echo -n $(pwd))" ]; then
+			cd -- "$(cat "$tempfile")"
+		fi
+	rm -f -- "$tempfile"
 }
 
 alias ranger=ranger-cd
+
+# Add a `r` function to zsh that opens ranger either at the given directory or
+# at the one zoxide suggests
+function r {
+	if [ "$1" != "" ]; then
+		if [ -d "$1" ]; then
+			ranger-cd "$1"
+		else
+			ranger-cd "$(zoxide query $1)"
+		fi
+	else
+		ranger-cd
+	fi
+	return $?
+}
